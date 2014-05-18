7e669dd7d7e0335e4427fdd6d927adaf31e947b3
Edoardo Negri <edoardo.negri@gmail.com>
2014-05-18 21:43:29 +0200

    livecoding test

--- visuals/test.py
+++ visuals/test.py
@@ -8,6 +8,8 @@ def loop():
 	identity()
 	linew(1)
 
+	push()
+
 	#p = Regular(3)
 	#p.r = 0.1
 	p = Polygon((0,0),(4*bass + 0.5, 0),(0.5,amp+0.1))
@@ -19,6 +21,8 @@ def loop():
 
 	rotate(time_rad())
 
+	pop()
+
 	p.fa = 0.5
 	p.ni = 10
 
@@ -26,8 +30,26 @@ def loop():
 	p.stroke.black()
 	p.h = hue + note/4
 
-	p.draw()
+	#p.draw()
 
-	#g = Grid(p, 2,2,1)
-	#g.r = 1
-	#g.draw()
+	c = Regular(8)
+	c.r = 0.5 + high
+
+	c.fill(1)
+	c.a = amp + 0.1
+	c.draw()
+
+	g = Grid(c, 3,3,1)
+	g.r = bass*20 + 0.1
+	g.draw()
+
+	c = Regular(4)
+	c.r = high
+
+	c.a = 1.0
+	c.fill.hsv()
+	c.h = hue + note/4
+
+	g = Grid(c, 3,3,1)
+	g.r = bass*20 + 0.1
+	g.draw()
