58cb9ac2d86742be726ba53e101c0d30397a7eb3
Edoardo Negri <edoardo.negri@gmail.com>
2014-06-17 23:33:35 +0200

    no access to graphic.import from livecoding

--- pineal/livecoding/imports.py
+++ pineal/livecoding/imports.py
@@ -2,5 +2,9 @@ from math import *
 from random import uniform, gauss
 import numpy as np
 
-from pineal.graphic.imports import *
 from color import *
+
+from OpenGL.GLUT import *
+import pyglet
+from pyglet.gl import *
+import thirdparty.ezpyinline as ezpyinline
