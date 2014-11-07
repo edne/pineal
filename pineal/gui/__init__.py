4dacb95cf6133d3761e153a369d4707fb868b28b
Edoardo Negri <edne@gmx.com>
2014-11-08 00:30:42 +0100

    gui with kivy

--- pineal/gui/__init__.py
+++ pineal/gui/__init__.py
@@ -1,63 +1,85 @@
 from time import sleep
-import gtk
 
 from pineal.osc import Osc
 from pineal.config import OSC_GUI, OSC_CORE
 
-from pineal.gui.widgets import VisualFrame, Menu
+import sys
+sys.argv = sys.argv[:1] + ['-ckivy:log_level:error']
+from kivy.app import App
+from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelHeader
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.slider import Slider as _Slider
 
 
-class Gui(object):
-    """Display a gui"""
-    def __init__(self):
-        self.win = gtk.Window()
-        self.win.set_title('Pineal Loop Project')
-        self.win.resize(200,1)
-        self.win.connect('delete-event', self.quit)
+class Slider(_Slider):
+    def __init__(self, visual, variable, **kwargs):
+        self.visual = visual
+        self.variable = variable
+        _Slider.__init__(self, **kwargs)
 
-        vbox = gtk.VBox()
-        menu = Menu(self)
-        self.box = gtk.HBox()
 
-        self.win.add(vbox)
-        vbox.add(menu)
-        vbox.add(self.box)
+class Tab(TabbedPanelHeader):
+    def __init__(self, **kwargs):
+        TabbedPanelHeader.__init__(self, **kwargs)
+        self.variables = []
 
-        self.win.show_all()
 
-        self.visuals = {}
-        self._stop = False
+class Gui(App):
+    def build(self):
+        self.tb= TabbedPanel()
+
+        while self.tb.tab_list:
+            self.tb.tab_list.pop(0)
+
+        return self.tb
 
     def run(self):
+        try:
+            App.run(self)
+        except KeyboardInterrupt:
+            self.on_stop()
+
+    def on_start(self):
         print 'starting pineal.gui'
+        self.visuals = {}
+
         self.osc = Osc(OSC_GUI, OSC_CORE)
         self.osc.listen('add', self.cb_add)
         self.osc.start()
 
-        try:
-            while not self._stop:
-                gtk.main_iteration()
-                sleep(0.01)  # don't ask me why
-        except KeyboardInterrupt:
-            self.osc.stop()
-
-    def stop(self):
-        print 'stopping pineal.gui'
-        self._stop = True
-
-    def quit(self, widget, event):
-        return not self._stop
+    def on_stop(self):
+        self.osc.stop()
 
     def cb_add(self, path, tags, args, source):
         visual, var, value = args
 
-        with gtk.gdk.lock:
-            if visual not in self.visuals.keys():
-                self.visuals[visual] = VisualFrame(self, visual)
-                self.box.add(self.visuals[visual])
+        if visual not in self.visuals.keys():
+            tab = Tab(text=visual)
+            tab.content = BoxLayout(orientation='horizontal')
+            self.tb.add_widget(tab)
 
-            if var not in self.visuals[visual].variables.keys():
-                self.visuals[visual].add_var(var)
-                self.visuals[visual].variables[var].change(value)
+            self.visuals[visual] = tab
 
-            self.box.show_all()
+        if var not in self.visuals[visual].variables:
+            tab = self.visuals[visual]
+            self.visuals[visual].variables.append(var)
+
+            slider = Slider(
+                visual, var,
+                min=0, max=1, value=value,
+                orientation='vertical', size_hint=(1, 0.9)
+            )
+            slider.bind(value=self.cb_slider)
+
+            label = Label(text=var, size_hint=(1, 0.1))
+
+            box = BoxLayout(orientation='vertical')
+            box.add_widget(slider)
+            box.add_widget(label)
+
+            tab.content.add_widget(box)
+
+    def cb_slider(self, slider, value):
+        self.osc.send('/visual/'+slider.visual+'/'+slider.variable, float(value))
