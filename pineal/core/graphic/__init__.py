9d4856e7954db7da2ab6037ccaa103b733c6e8f0
Edoardo Negri <edne@gmx.com>
2014-11-25 21:43:23 +0100

    starting kivy-ization

--- pineal/core/graphic/__init__.py
+++ pineal/core/graphic/__init__.py
@@ -1,18 +1,19 @@
-import OpenGL.GLUT as glut
-import pyglet.gl as gl
-import pyglet
+import sys
+sys.argv = sys.argv[:1] + ['-ckivy:log_level:error', '--size=640x480']
+import kivy.graphics.opengl as gl
 
 import windows
 
 
+# TODO Graphic as kivy App
 class Graphic:
     def __init__(self, visuals):
         windows.init(visuals)
 
     def update(self):
-        dt = pyglet.clock.tick()
+        #dt = pyglet.clock.tick()
         windows.update()
-        
+
     def run(self):
         try:
             while True:
