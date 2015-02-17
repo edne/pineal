104bd74cd851befd6c191132c2a4f2e5e7443b80
Edoardo Negri <edne@gmx.com>
2015-02-17 22:13:34 +0100

    random170215 example

---
+++ visions/examples/random170215.py
@@ -0,0 +1,37 @@
+from lib.graphic import *
+import lib.audio as audio
+from math import *
+from random import *
+from time import time
+
+amp = audio.source("AMP")
+bass = audio.source("(LPF 100) AMP")
+high = audio.source("(HPF 10000) AMP")
+
+p = Polygon(30)
+frame = Frame()
+
+
+@turnaroud(6)
+def a():
+    translate(0.1 + amp())
+    p.r = 0.04
+    p.fill = hsv(time())
+    p.stroke = rgb(0,0)
+
+    p.draw()
+
+
+def realmagic():
+    frame.r = 0.98 + amp()
+    frame.draw()
+    frame.r = 0.5
+    frame.draw()
+
+
+def draw():
+    strokeWeight(4)
+
+    realmagic()
+    rotate(time2rad(2))
+    a()
