5b84f4ece1e9f237ecbc99d8be39aa5833e25fd5
Edoardo Negri <edne@gmx.com>
2015-05-24 22:01:27 +0200

    new example

--- visions/test.py
+++ visions/test.py
@@ -2,52 +2,33 @@ from lib.graphic import *
 import lib.audio as audio
 from math import *
 from random import *
-#from time import time
 
 amp = audio.source("AMP")
-bass = audio.source("(LPF 100) AMP")
-high = audio.source("(HPF 10000) AMP")
 p = Polygon(4)
-q = Polygon(20)
 frame = Frame()
-
-
-@turnaround(2, 0.5)
-@pushmatrix
-def a():
-    p.r = 0.4
-    translate(bass()*4)
-
-    p.fill = rgb(1, 0.5)
-    p.stroke = rgb(0, 1)
-
-    p.draw()
 
 
 @turnaround(2)
 @pushmatrix
-def b():
-    q.fill = hsv(0.6, 1)
-    q.stroke = rgb(0)
-    translate(4*amp())
-    scale(0.1)
-    q.draw()
+def a():
+    translate(0.5)
+    scale(0.4)
+
+    c = 8*amp()
+    p.fill = rgb(1, c)
+    p.stroke = rgb(1 - c, c)
+
+    p.draw()
 
 
-@turnaround(13)
+@turnaround(23)
 def feedback():
-    frame.r = 0.5
-    frame.x = 0.5
-
+    frame.r = 0.9
     frame.draw()
 
 
 def draw():
     strokeWeight(4)
 
-    rotate(time2rad())
-    a()
-    rotate(time2rad(-2))
-    b()
-
     feedback()
+    a()
