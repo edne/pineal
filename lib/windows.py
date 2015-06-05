564df56a58adb062af720861a6dca75fb2031adc
Edoardo Negri <edne@gmx.com>
2015-06-05 19:42:42 +0200

    from classes to clusures

--- lib/windows.py
+++ lib/windows.py
@@ -11,46 +11,48 @@ def getRenderTexture():
     return render_texture
 
 
-class Window(pyglet.window.Window):
-    def update(self):
-        self.switch_to()
-        self.dispatch_events()
-        self.dispatch_event('on_draw')
-        self.flip()
+def Window(*args, **kwargs):
+    window = pyglet.window.Window(*args, **kwargs)
+
+    def update():
+        window.switch_to()
+        window.dispatch_events()
+        window.dispatch_event('on_draw')
+        window.flip()
+    window.update = update
+    return window
 
 
-class Renderer(Window):
+def Renderer(visions):
     """
     Offscreen windows where render stuff
     """
-    def __init__(self, visions):
-        self.visions = visions
+    W, H = RENDER_SIZE
+    window = Window(caption='(pineal renderer)',
+                    fullscreen=0,
+                    width=W,
+                    height=H,
+                    vsync=0,  # TODO: set to 1 after removing the while 1
+                    visible=0)
 
-        W, H = RENDER_SIZE
-        Window.__init__(self,
-                        caption='(pineal renderer)',
-                        fullscreen=0,
-                        width=W,
-                        height=H,
-                        vsync=0,  # TODO: set to 1 after removeing the while 1
-                        visible=0)
+    window.visions = visions
 
-        gl.glEnable(gl.GL_BLEND)
-        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+    gl.glEnable(gl.GL_BLEND)
+    gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
 
-        gl.glEnable(gl.GL_LINE_SMOOTH)
-        gl.glEnable(gl.GL_POLYGON_SMOOTH)
-        gl.glHint(gl.GL_LINE_SMOOTH_HINT, gl.GL_NICEST)
-        gl.glHint(gl.GL_POLYGON_SMOOTH_HINT, gl.GL_NICEST)
+    gl.glEnable(gl.GL_LINE_SMOOTH)
+    gl.glEnable(gl.GL_POLYGON_SMOOTH)
+    gl.glHint(gl.GL_LINE_SMOOTH_HINT, gl.GL_NICEST)
+    gl.glHint(gl.GL_POLYGON_SMOOTH_HINT, gl.GL_NICEST)
 
-        self.texture = None
+    window.texture = None
 
-    def on_draw(self):
-        self.clear()
+    def on_draw():
+        window.clear()
 
         gl.glMatrixMode(gl.GL_PROJECTION)
         gl.glLoadIdentity()
-        (w, h) = self.get_size()
+        (w, h) = window.get_size()
         gl.glScalef(
             float(min(w, h))/w,
             -float(min(w, h))/h,
@@ -63,78 +65,84 @@ class Renderer(Window):
                      0, 1, 0)
 
         global render_texture
-        render_texture = self.texture
+        render_texture = window.texture
 
-        for v in self.visions.values():
+        for v in window.visions.values():
             gl.glMatrixMode(gl.GL_MODELVIEW)
             gl.glLoadIdentity()
             v.iteration()
 
         buf = pyglet.image.get_buffer_manager().get_color_buffer()
         rawimage = buf.get_image_data()
-        self.texture = rawimage.get_texture()
+        window.texture = rawimage.get_texture()
 
         clock.tick()
+    window.on_draw = on_draw
 
-    def update(self):
-        Window.update(self)
+    old_update = window.update
+
+    def update():
+        old_update()
         print '\rfps: %3.1f' % clock.get_fps(),
+    window.update = update
+
+    return window
 
 
-class OutputWindow(Window):
-    def on_draw(self):
-        w, h = self.width, self.height
+def OutputWindow(*args, **kwargs):
+    window = Window(*args, **kwargs)
+
+    def on_draw():
+        w, h = window.width, window.height
         side = max(w, h)
 
-        self.clear()
-        if self.source.texture:
-            self.source.texture.blit(-(side-w)/2,
-                                     -(side-h)/2,
-                                     0,
-                                     side, side)
+        window.clear()
+        if window.source.texture:
+            window.source.texture.blit(-(side-w)/2,
+                                       -(side-h)/2,
+                                       0,
+                                       side, side)
+
+    window.on_draw = on_draw
+    return window
 
 
-class Master(OutputWindow):
+def Master(source):
     """
     Master output, on secondary monitor is exists, hidden otherwise
     """
-    def __init__(self, source):
-        self.source = source
+    platform = pyglet.window.get_platform()
+    display = platform.get_default_display()
+    screens = display.get_screens()
 
-        platform = pyglet.window.get_platform()
-        display = platform.get_default_display()
-        screens = display.get_screens()
-
-        # TODO: refactor this with a second monitor
-        W, H = RENDER_SIZE
-        if len(screens) > 1:
-            Window.__init__(self,
-                            caption='(pineal master)',
-                            fullscreen=1,
-                            screen=screens[-1],
-                            vsync=1,
-                            visible=1)
-            self.set_mouse_visible(False)
-        else:
-            Window.__init__(self,
-                            caption='(pineal master)',
-                            fullscreen=0,
-                            width=W,
-                            height=H,
-                            vsync=1,
-                            visible=0)
-        #
+    # TODO: refactor this with a second monitor
+    W, H = RENDER_SIZE
+    if len(screens) > 1:
+        window = Window(caption='(pineal master)',
+                        fullscreen=1,
+                        screen=screens[-1],
+                        vsync=1,
+                        visible=1)
+        window.set_mouse_visible(False)
+    else:
+        window = Window(caption='(pineal master)',
+                        fullscreen=0,
+                        width=W,
+                        height=H,
+                        vsync=1,
+                        visible=0)
+    #
+    window.source = source
+    return window
 
 
-class Overview(OutputWindow):
+def Overview(source):
     """
     Overview for the programmer, nothing else to say
     """
-    def __init__(self, source):
-        self.source = source
-
-        Window.__init__(self,
-                        resizable=True,
-                        caption='(pineal overview)',
-                        width=600, height=450,
-                        vsync=0)
+    window = OutputWindow(resizable=True,
+                          caption='(pineal overview)',
+                          width=600, height=450,
+                          vsync=0)
+    window.source = source
+    return window
