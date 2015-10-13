b6d10a4a85d86825607d7bba6ad9367bbd09a792
Edoardo Negri <edne@gmx.com>
2015-10-13 12:41:45 +0200

    changhed docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.fulltoc']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -108,9 +108,16 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    html_theme = 'default'
+else:
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
+
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
