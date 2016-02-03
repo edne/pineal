a757cf8f801394590c027525ac1144a7ca1b2931
Edoardo Negri <edne@gmx.com>
2016-02-03 23:00:02 +0100

    Coherent coding style

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,21 +1,19 @@
 #include "ofApp.h"
 #include <boost/python.hpp>
 
-using namespace boost::python;
+namespace py = boost::python;
 
-void background(double x){
-    ofBackground( x * 255 );
+void background(double x) {
+    ofBackground(x * 255);
 }
 
-BOOST_PYTHON_MODULE(core)
-{
-    def( "background", &background );
+BOOST_PYTHON_MODULE(core) {
+    py::def("background", &background);
 }
 
-object ns;
+py::object ns;
 
-//--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
 
     ofLog() << "Running setup()";
 
@@ -23,100 +21,55 @@ void ofApp::setup(){
 
     try {
         Py_Initialize();
-        PySys_SetArgv( argc, argv );
-        PyImport_AppendInittab( "core", &initcore );
+        PySys_SetArgv(argc, argv);
+        PyImport_AppendInittab("core", &initcore);
 
-        ns = import( "__main__" ).attr( "__dict__" );
+        ns = py::import("__main__").attr("__dict__");
 
-        exec( "import hy", ns );
-        exec( "from py.hy_utils import eval_hy_code", ns );
-        exec( "dsl_ns = {}", ns );
-        exec( "dsl_history = []", ns );
+        py::exec("import hy", ns);
+        py::exec("from py.hy_utils import eval_hy_code", ns);
+        py::exec("dsl_ns = {}", ns);
+        py::exec("dsl_history = []", ns);
 
         // TODO take code from OSC
-        exec( "dsl_history.append('(import [core [*]])(defn --draw-- [] (background 0.2))')", ns );
-        exec( "eval_hy_code(dsl_history[-1], dsl_ns)", ns );
+        py::exec("dsl_history.append('(import [core [*]])(defn --draw-- [] (background 0.2))')", ns);
+        py::exec("eval_hy_code(dsl_history[-1], dsl_ns)", ns);
 
-    } catch( error_already_set ) {
+    } catch(py::error_already_set) {
         PyErr_Print();
     }
 }
 
-//--------------------------------------------------------------
-void ofApp::update(){
-    while(oscReceiver.hasWaitingMessages()){
+void ofApp::update() {
+    while (oscReceiver.hasWaitingMessages()) {
 		// get the next message
 		ofxOscMessage m;
 		oscReceiver.getNextMessage(m);
 
-        if(m.getAddress() == "/code"){
+        if (m.getAddress() == "/code") {
             string code = m.getArgAsString(0);
             ofLog() << "/code " << code;
 		}
     }
 }
 
-//--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
     try {
         // TODO check history
-        exec( "dsl_ns['__draw__']()", ns );
-    } catch( error_already_set ) {
+        py::exec("dsl_ns['__draw__']()", ns);
+    } catch (py::error_already_set) {
         PyErr_Print();
     }
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
+void ofApp::keyPressed(int key) {}
+void ofApp::keyReleased(int key) {}
+void ofApp::mouseMoved(int x, int y) {}
+void ofApp::mouseDragged(int x, int y, int button) {}
+void ofApp::mousePressed(int x, int y, int button) {}
+void ofApp::mouseReleased(int x, int y, int button) {}
+void ofApp::mouseEntered(int x, int y) {}
+void ofApp::mouseExited(int x, int y) {}
+void ofApp::windowResized(int w, int h) {}
+void ofApp::gotMessage(ofMessage msg) {}
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
