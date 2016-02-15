44a4771af98a0d24e5796dfc707afd5b8c59e396
Edoardo Negri <edne@gmx.com>
2016-02-15 22:16:05 +0100

    Add scale, translate and rotate

--- src/pnEmbed.cpp
+++ src/pnEmbed.cpp
@@ -1,6 +1,9 @@
 #include "ofApp.h"
 
 namespace dsl{
+	ofColor status_color;
+	bool status_fill = true;
+
 	void of_log(string s){
 		ofLog() << s;
 	}
@@ -13,14 +16,57 @@ namespace dsl{
 		ofDrawBox(r);
 	}
 
-	void scale(py::object f, double r){
+	void scale_xyz(py::object f, double x, double y, double z){
 		ofPushMatrix();
-		ofScale(r, r, r);
+		ofScale(x, y, z);
 		f();
 		ofPopMatrix();
 	}
 
-	ofColor status_color;
+	void scale_xy(py::object f, double x, double y){
+		scale_xyz(f, x, y, 1);
+	}
+
+	void scale_r(py::object f, double r){
+		scale_xyz(f, r, r, r);
+	}
+
+	void translate_xyz(py::object f, double x, double y, double z){
+		ofPushMatrix();
+		ofTranslate(x, y, z);
+		f();
+		ofPopMatrix();
+	}
+
+	void translate_xy(py::object f, double x, double y){
+		translate_xyz(f, x, y, 0);
+	}
+
+	void translate_x(py::object f, double x){
+		translate_xyz(f, x, 0, 0);
+	}
+
+	void rotate_x(py::object f, double rad){
+		ofPushMatrix();
+		ofRotateX(180 * rad / PI);
+		f();
+		ofPopMatrix();
+	}
+
+	void rotate_y(py::object f, double rad){
+		ofPushMatrix();
+		ofRotateY(180 * rad / PI);
+		f();
+		ofPopMatrix();
+	}
+
+	void rotate_z(py::object f, double rad){
+		ofPushMatrix();
+		ofRotateZ(180 * rad / PI);
+		f();
+		ofPopMatrix();
+	}
+
 	void color(py::object f, double r, double g, double b){
 		ofColor old_color = status_color;
 		ofColor new_color = ofColor(r * 255, g * 255, b * 255);
@@ -34,7 +80,6 @@ namespace dsl{
 		ofSetColor(status_color);
 	}
 
-	bool status_fill = true;
 	void _fill(py::object f, bool status){
 		bool old_fill = status_fill;
 		bool new_fill = status;
@@ -68,7 +113,19 @@ namespace dsl{
 		py::def("of_log", &of_log);
 		py::def("background", &background);
 		py::def("cube", &cube);
-		py::def("scale", &scale);
+
+		py::def("scale", &scale_r);
+		py::def("scale", &scale_xy);
+		py::def("scale", &scale_xyz);
+
+		py::def("translate", &translate_x);
+		py::def("translate", &translate_xy);
+		py::def("translate", &translate_xyz);
+
+		py::def("rotate_x", &rotate_x);
+		py::def("rotate_y", &rotate_y);
+		py::def("rotate_z", &rotate_z);
+
 		py::def("color", &color);
 		py::def("fill", &fill);
 		py::def("no_fill", &no_fill);
