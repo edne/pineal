97d40c8c77a82e06ab44bcb6e8091792a20c8fcf
Edoardo Negri <edne@gmx.com>
2016-02-25 23:19:09 +0100

    Add polygons

--- src/dsl/core.h
+++ src/dsl/core.h
@@ -11,9 +11,43 @@ void cube(double r){
 	ofDrawBox(r);
 }
 
+unordered_map<int, shared_ptr<ofPolyline>> polygons;
+
+void _polygon(int n, float r){
+	shared_ptr<ofPolyline> p;
+
+	if(polygons.find(n) == polygons.end()){
+		p = make_shared<ofPolyline>();
+
+		float angle, x, y;
+
+		for(int i = 0; i < n; i++){
+			angle = PI / 2 + i * TWO_PI / n;
+			x = cos(angle);
+			y = sin(angle);
+			p->addVertex(ofPoint(x,y));
+		}
+		p->close();
+
+		polygons[n] = p;
+	}else{
+		p = polygons[n];
+	}
+	ofPushMatrix();
+	ofScale(r, r, r);
+	p->draw();
+	ofPopMatrix();
+}
+
+void polygon(int n){
+	_polygon(n, 1);
+}
+
 void def_core(){
 	py::def("of_log", &of_log);
 	py::def("cube", &cube);
+	py::def("polygon", &_polygon);
+	py::def("polygon", &polygon);
 
 	def_layers();
 	def_colors();
