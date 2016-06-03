33d9827b2d8e044dc400859e7e36f1c2ae851c1a
Edoardo Negri <edne@gmx.com>
2016-06-03 14:11:48 +0200

    Remove Client class

--- src/client.py
+++ src/client.py
@@ -9,9 +9,15 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import liblo
 
+readline.parse_and_bind("tab: complete")
+
 logger = logging.getLogger("frontend.py")
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
+
+server_addr = os.environ.get("SERVER_ADDR", "127.0.0.1:7172")
+listen_port = os.environ.get("LISTEN_PORT", 7173)
+listen_port = int(listen_port)
 
 
 def watch_file(file_name, action, *args, **kwargs):
@@ -51,140 +57,153 @@ def check_address(addr):
         return ":".join(["localhost", splitted[0]])
 
 
-class Client(object):
-    def __init__(self, server_addr, listen_port):
-        "Init frontend"
-        logger.info("Starting frontend")
-        self.server = liblo.Address("osc.udp://{}/"
-                                    .format(check_address(server_addr)))
-        self.running = False
-        self.server_responding = False
-        self.server_errors = False
-        self.commands = {}
+class State(dict):
+    "State class, like a dict but with . notation"
 
-        readline.parse_and_bind("tab: complete")
+    def __getattr__(self, name):
+        "Get dict field as attribute"
+        return self[name]
 
-    def add_command(self, name, function=None):
-        "Add a new command to the command line interface"
-        if function:
-            self.commands[name] = function
-        else:
-            # used as decorator
-            def decorator(f):
-                self.commands[name] = f
-                return f
-            return decorator
-
-    def handle_osc(self, path, msg):
-        "Handle OSC events"
-        if path == "/ack":
-            logger.info("/ack received, sending code")
-            self.server_responding = True
-
-        if path == "/status/error":
-            logger.error("/error received: {}".format(msg[0]))
-            self.server_errors = True
-
-        if path == "/status/working":
-            self.server_errors = False
-
-    def handle_command(self, command, *args):
-        "Handle user commands"
-        if command in self.commands:
-            self.commands[command](*args)
-
-    def send(self, path, *msg):
-        "Send message on a given path"
-        logger.info("Sending to {0}".format(path))
-        liblo.send(self.server, path, *msg)
-
-    def main_loop(self):
-        "Keep looping"
-
-        try:  # future proof
-            _input = raw_input
-        except NameError:
-            _input = input
-
-        self.running = True
-        try:
-            while self.running:
-                line = _input("> ").split()
-                if line:
-                    command = line[0]
-                    args = line[1:]
-                    self.handle_command(command, *args)
-        except KeyboardInterrupt:
-            logger.info("\rClosed with ^C")
-            self.handle_command("exit")
+    def __setattr__(self, name, value):
+        "Set dict field as attribute"
+        self[name] = value
 
 
-server_addr = os.environ.get("SERVER_ADDR", "127.0.0.1:7172")
-listen_port = os.environ.get("LISTEN_PORT", 7173)
-listen_port = int(listen_port)
+state = State()
+state.running = False
+state.server_responding = False
+state.server_errors = False
 
-client = Client(server_addr, listen_port)
-
+commands = {}
 watchers = []
 
-listener = liblo.ServerThread(listen_port)
-listener.add_method(None, None, client.handle_osc)
-listener.start()
+
+def handle_osc(path, msg):
+    "Handle OSC events"
+    if path == "/ack":
+        logger.info("/ack received, sending code")
+        state.server_responding = True
+
+    if path == "/status/error":
+        logger.error("/error received: {}".format(msg[0]))
+        state.server_errors = True
+
+    if path == "/status/working":
+        state.server_errors = False
 
 
-@client.add_command("ping")
+def add_command(name, function=None):
+    "Add a new command to the command line interface"
+    if function:
+        commands[name] = function
+    else:
+        def decorator(f):
+            "When add_command is used as decorator"
+            commands[name] = f
+            return f
+        return decorator
+
+
+def handle_command(command, *args):
+    "Handle user commands"
+    if command in commands:
+        commands[command](*args)
+
+
+@add_command("send")
+def send(path, *msg):
+    "Send message on a given path"
+    logger.info("Sending to {0}".format(path))
+    addr = liblo.Address("osc.udp://{}/"
+                         .format(check_address(server_addr)))
+    liblo.send(addr, path, *msg)
+
+
+@add_command("ping")
 def ping():
     "Send /ping to the server untill /ack response"
     logger.info("sending /ping messages to the server")
-    while not client.server_responding:
-        client.send("/ping")
+    while not state.server_responding:
+        send("/ping")
         sleep(1)
 
 
-@client.add_command("run")
+@add_command("run")
 def run(file_name):
     "Read a file and send its content to /run-code"
     with open(file_name) as f:
         logger.info("Sending content of {}".format(file_name))
-        client.send("/run-code", f.read())
+        send("/run-code", f.read())
 
 
-@client.add_command("watch")
+@add_command("watch")
 def watch(file_name):
     "Watch for changes in a file and send it to /run-code each time"
     w = watch_file(file_name,
-                   lambda: client.run(file_name))
+                   lambda: run(file_name))
     watchers.append(w)
     run(file_name)
 
 
-@client.add_command("exit")
+@add_command("exit")
 def exit():
     "Ask the server to exit, stop listening and stop all the watchers"
-    client.running = False
-    client.send("/exit")
+    state.running = False
+    send("/exit")
 
 
-@client.add_command("test")
+@add_command("test")
 def test(*files):
     "Test given files"
     for file_name in files:
         run(file_name)
         sleep(1)
-        if client.server_errors:
+        if state.server_errors:
             logger.error("Errors in file {}".format(file_name))
-            client.send("/exit")
+            send("/exit")
             sys.exit(1)
-    client.send("/exit")
+    send("/exit")
+
+
+def main_loop():
+    "Keep looping"
+
+    try:  # future proof
+        _input = raw_input
+    except NameError:
+        _input = input
+
+    state.running = True
+    try:
+        while state.running:
+            line = _input("> ").split()
+            if line:
+                command = line[0]
+                args = line[1:]
+                handle_command(command, *args)
+    except KeyboardInterrupt:
+        logger.info("\rClosed with ^C")
+        exit()
 
 
 def main():
     "Main function"
+    listener = liblo.ServerThread(listen_port)
+    listener.add_method(None, None, handle_osc)
+    listener.start()
+
     if sys.argv[1:]:
         ping()
-        client.handle_command(*sys.argv[1:])
+        handle_command(*sys.argv[1:])
     else:
-        client.main_loop()
+        main_loop()
+        if watchers:
+            for w in watchers:
+                w.stop()
+            for w in watchers:
+                w.join()
+
+    listener.stop()
 
 
 if __name__ == "__main__":
