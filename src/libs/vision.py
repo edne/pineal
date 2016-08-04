dbebdd3e3a984a0263e6eedf9ce3ce280fd18a36
Edoardo Negri <edne@gmx.com>
2016-08-04 13:42:17 +0200

    Remove utils.py

--- src/libs/vision.py
+++ src/libs/vision.py
@@ -1,5 +1,19 @@
 from __future__ import print_function
-from libs.utils import hy_eval_code
+from hy.lex import tokenize
+from hy.compiler import hy_compile
+from hy.importer import ast_compile
+
+
+def hy_eval_code(source, namespace):
+    "Eval Hy code from string"
+
+    if "__name__" not in namespace:
+        namespace["__name__"] = __name__
+
+    tokens = tokenize(source)
+    ast = hy_compile(tokens, "__pineal__")
+    code = ast_compile(ast, "", "exec")
+    eval(code, namespace)
 
 
 class Vision(object):
