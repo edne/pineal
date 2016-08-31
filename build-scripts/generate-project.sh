724ff4f124752598aa4ea6e7c7344acfac7721c5
Edoardo Negri <edne@gmx.com>
2016-08-31 22:39:13 +0200

    Add build scripts

---
+++ build-scripts/generate-project.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm Makefile
+$HOME/openframeworks/apps/projectGenerator/commandLine/bin/projectGenerator -a"ofxOsc, ofxAubio" -o"$HOME/openframeworks" .
---
+++ build-scripts/install-of.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+OF_VERSION=0.9.0
+
+if [ ! -d $HOME/openframeworks ]; then
+    wget http://openframeworks.cc/versions/v${OF_VERSION}/of_v${OF_VERSION}_linux64_release.tar.gz
+    tar xvf of_v${OF_VERSION}_linux64_release.tar.gz
+    mv of_v${OF_VERSION}_linux64_release openframeworks
+    rm of_v${OF_VERSION}_linux64_release.tar.gz
+
+    sed -i "s/\#include \"RtAudio\.h\"/\#include \"rtaudio\/RtAudio\.h\"/g" openframeworks/libs/openFrameworks/sound/ofRtAudioSoundStream.cpp
+
+    sudo openframeworks/scripts/linux/debian/install_dependencies.sh
+    sudo openframeworks/scripts/linux/debian/install_codecs.sh
+    openframeworks/scripts/linux/compileOF.sh
+
+    mv openframeworks $HOME
+
+    pushd $HOME/openframeworks/apps/projectGenerator/commandLine
+    make
+    popd
+
+    pushd pushd $HOME/openframeworks/addond
+    git clone https://github.com/aubio/ofxAubio
+    popd
+fi
