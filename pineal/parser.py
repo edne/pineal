0c65d901f86095d41268cd1269d87b8e1685a370
Edoardo Negri <edne@gmx.com>
2017-04-25 16:23:57 +0200

    Draw list of entities

--- pineal/parser.py
+++ pineal/parser.py
@@ -4,18 +4,58 @@ from tools import polygon
 
 log = logging.getLogger(__name__)
 
+
+class ParserError(Exception):
+    pass
+
+
 primitives = {'polygon': polygon}
+
+
+def get_entities(tree):
+    entities = []
+
+    # tree: [{'polygon': {'sides': 4, ...}}, ...]
+    for branch in tree:
+        # branc.items(): [('polygon', {...})]
+        name, kwargs = branch.items()[0]
+
+        if name in primitives:
+            # TODO: check in namespace or in a symbol table
+            entity = primitives[name](**kwargs)
+            entities.append(entity)
+
+    return entities
+
+
+def parse_draw(tree, namespace):
+    if not isinstance(tree, list):
+        raise ParserError('Draw should take a list of items')
+
+    entities = get_entities(tree)
+
+    def draw():
+        for entity in entities:
+            entity()
+
+    namespace['draw'] = draw
+
+
+def parse_top_level(tree, namespace):
+    # TODO:
+    # module
+    # osc-in
+    # palette
+    # parse_definitions(tree, namespace)  # layer, group
+
+    if 'draw' not in tree:
+        raise ParserError('Missing draw entry')
+    else:
+        parse_draw(tree['draw'], namespace)
 
 
 def parse(code, namespace):
     tree = yaml.load(code)
     log.debug(tree)
 
-    def draw():
-        for branch in tree:
-            if branch in primitives:
-                kwargs = tree[branch]
-                entity = primitives[branch](**kwargs)
-                entity()
-
-    namespace['draw'] = draw
+    parse_top_level(tree, namespace)
