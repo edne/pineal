4bb127ffc5e506f378a12718053e4960a5784c54
Edoardo Negri <edne@gmx.com>
2017-04-26 17:43:20 +0200

    Add groups

--- pineal/parser.py
+++ pineal/parser.py
@@ -1,5 +1,5 @@
 import logging
-from tools import apply_effects
+from tools import apply_effects, group
 from tools import polygon, scale
 from pineal.tree_parser import make_tree
 
@@ -15,29 +15,44 @@ _primitives = {'polygon': polygon}
 _effects = {'scale': scale}
 
 
-def make_effect(branch):
+def make_effect(branch, namespace):
     name, leaf = branch
     arg = eval(leaf)
     effect = _effects[name](arg)
     return effect
 
 
+def make_group(tree, namespace):
+    log.debug('Group tree: {}'.format(tree))
+    entities = [make_entity(branch, namespace)
+                for branch in tree]
+    return group(entities)
+
+
 def make_entity(tree, namespace):
     log.debug(tree)
     name, body = tree
 
-    effects = [make_effect(branch)
+    effects = [make_effect(branch, namespace)
                for branch in body
                if branch[0] in _effects]
 
-    kwargs = {key: eval(value)
-              for (key, value) in body
-              if key not in _effects}
+    body = [(key, value)
+            for (key, value) in body
+            if key not in _effects]
 
     if name in _primitives:
+        kwargs = {key: eval(value)
+                  for (key, value) in body}
+
         entity = _primitives[name](**kwargs)
+
+    elif name == 'group':
+        # TODO: named groups
+        entity = make_group(body, namespace)
+
     else:
-        # TODO: make groups and layers
+        # TODO: layers
         raise ParserError('Invalid entity')
 
     return apply_effects(entity, effects)
