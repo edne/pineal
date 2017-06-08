5f809b3433f5322f3caca793e82781b79f699dbe
Edoardo Negri <edne@gmx.com>
2017-06-08 02:32:48 +0200

    Move renderer

--- pineal/renderer.py
+++ pineal/renderer.py
@@ -1,10 +1,41 @@
 import logging
 from contextlib import contextmanager
 import pyglet
+import pyglet.gl as gl
 import pineal.osc as osc
-from pineal.windows import new_renderer
 
 log = logging.getLogger(__name__)
+
+
+def rendering_window(draw, h, w):
+    window = pyglet.window.Window(caption='(pineal renderer)',
+                                  width=w,
+                                  height=h,
+                                  visible=False)
+
+    gl.glEnable(gl.GL_BLEND)
+    gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+
+    gl.glEnable(gl.GL_LINE_SMOOTH)
+    gl.glEnable(gl.GL_POLYGON_SMOOTH)
+    gl.glHint(gl.GL_LINE_SMOOTH_HINT, gl.GL_NICEST)
+    gl.glHint(gl.GL_POLYGON_SMOOTH_HINT, gl.GL_NICEST)
+
+    window.texture = None
+
+    @window.event
+    def on_draw():
+        pyglet.clock.tick()
+        window.clear()
+
+        gl.glLoadIdentity()
+        draw()
+
+        buf = pyglet.image.get_buffer_manager().get_color_buffer()
+        rawimage = buf.get_image_data()
+        window.texture = rawimage.get_texture()
+
+    return window
 
 
 def pineal_eval(code, ns):
@@ -44,7 +75,7 @@ def render():
         with safety(stack, namespace):
             namespace['draw']()
 
-    new_renderer(draw, [800, 800])
+    rendering_window(draw, 800, 800)
 
     osc.add_callback('/eye/code', callback)
     osc.start_server()
