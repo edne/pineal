1450ada356bcd5f8be87820d4036908a2f15e723
Edoardo Negri <edne@gmx.com>
2017-06-10 18:09:42 +0200

    Take file_name from command line

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-file_name = "test.py"
 log_level = "debug"
 
 osc_addr = 'localhost', 1111
--- pineal.py
+++ pineal.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from sys import argv, exit
 from time import sleep
 import logging
 
@@ -15,9 +16,14 @@ logging.basicConfig(level=(logging.DEBUG
 
 
 if __name__ == '__main__':
+    if len(argv) < 2:
+        print('Usage:', argv[0], 'file.py')
+        exit(1)
+
+    file_name = argv[1]
+
     osc_addr = config.osc_addr
     _, port = osc_addr
-    file_name = config.file_name
 
     receive(port)
     render(file_name)
